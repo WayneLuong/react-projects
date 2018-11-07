3cd5b3533510d79a4bffd6a18be2e5913ff2a8d1
Wayne Luong <wayneluong93@gmail.com>
2018-11-07 21:06:51 +0000

    Add files via upload

---
+++ react-switch-form/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import {Userform} from './components/Userform'
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+        <Userform/>
+        </div>
+    );
+  }
+}
+
+export default App;
